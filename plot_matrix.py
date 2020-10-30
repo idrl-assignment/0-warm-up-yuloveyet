3fea1c0ff51c6990ddf35aaaf4a3f3640ba7a484
yuloveyet <liyu@mail.nwpu.edu.cn>
2020-10-30 08:17:15 +0800

    update

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,7 +2,7 @@ import matplotlib.image as mpimg
 import numpy as np
 
 def generate_random_matrix(m, n):
-    return np.random.randint(m, n)
+    return np.random.randint(0,2, size = (m, n))
 
 def save_matrix(matrix, file_name):
     mpimg.imsave(file_name, matrix)
@@ -12,3 +12,5 @@ def save_matrix(matrix, file_name):
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
     save_matrix(matrix, "example.jpg")
+
+print(matrix)
